c66176d728f89515315c45ed7d50059b387d81a2
Niklas Albinsson <albinssonniklas@gmail.com>
2018-03-12 13:11:44 +0100

    contactform igen

--- src/layouts/ContactForm.js
+++ src/layouts/ContactForm.js
@@ -2,25 +2,48 @@ import React, { Component } from "react";
 import FaMail from "react-icons/lib/fa/envelope";
 
 class ContactForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	handleChange = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	handleSubmit = e => {
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: encode({ "form-name": "contact", ...this.state })
+		})
+			.then(() => alert("Success!"))
+			.catch(error => alert(error));
+
+		e.preventDefault();
+	};
+
 	render() {
 		return (
 			<form
 				name="contact"
-				id="contact-form"
-				method="POST"
+				method="post"
+				action="/thanks/"
 				data-netlify="true"
+				data-netlify-honeypot="bot-field"
+				onSubmit={this.handleSubmit}
 			>
 				<div className={"contact-form-section"}>
 					<label>Ditt namn:</label>
-					<input required type="text" name="name" />
+					<input required type="text" name="name" onChance={this.handleChange}/>
 				</div>
 				<div className={"contact-form-section"}>
 					<label>Din mail:</label>
-					<input required type="email" name="email" />
+					<input required type="email" name="email" onChance={this.handleChange}/>
 				</div>
 				<div className={"contact-form-section"}>
 					<label>Ditt meddelande:</label>
-					<textarea required name="message" cols="30" rows="3" />
+					<textarea required name="message" cols="30" rows="3" onChance={this.handleChange}/>
 				</div>
 				<div netlify-recaptcha />
 				<div className={"contact-form-section"}>
